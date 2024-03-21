65e297fa6102b8eb0ac2fa2fa86827e4e599dde1
pAx24 <84711602+pAx24@users.noreply.github.com>
2024-03-21 09:55:08 +0200

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,8 +1,11 @@
 #include "lib/terminal/terminal.h"
 #include "lib/filesystem/filesystem.h"
+#include "lib/memory/memory.h"
 #include "bin/programmenu.c"
 
 void kernel_main(void) {
+	mmu_init();
+	gdt_init();
 	terminal_init();
 	keyboard_init();
 	programmenu_main();
