0743bc4b56507da9163f73a1349a5d42ee9b1077
Momchil Kalestrov <kalestrov.mo@gmail.com>
2024-07-30 22:54:23 +0300

    fix get/set system calls

--- kernel/syscalls/syscall.c
+++ kernel/syscalls/syscall.c
@@ -8,12 +8,12 @@ void _handle_syscall(uint32_t eax, uint32_t ebx, uint32_t ecx) {
             write_calls[syscall_type](eax, ebx, ecx);
             return;
         case COMMAND_SET:
-            uint32_t id = eax & 0x00ffffff;
-            *(variables[id]) = ebx;
+            uint32_t id_set = eax & 0x00ffffff;
+            *(uint32_t *)(variables[id_set]) = ebx;
             return;
         case COMMAND_GET:
-            uint32_t id = eax & 0x00ffffff;
-            return_value = *(variables[id]);
+            uint32_t id_get = eax & 0x00ffffff;
+            return_value = *(uint32_t *)(variables[id_get]);
             has_return = true;
             return;
     }
